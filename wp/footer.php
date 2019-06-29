b703dbe966a39300ce9fca9a4b2e712709a3f509
miyamoto <m.akirabo@gmail.com>
2019-06-29 14:32:44 +0900

    ヘッダー・フッターを外部ファイル化ける

---
+++ wp/footer.php
@@ -0,0 +1,23 @@
+
+<footer class="main-footer">
+  <div class="footer_gmenu">
+ 
+  </div>
+  <ul class="footer_gnavi">
+    <li><a href="">トップ</a></li>
+    <li><a href="">サービス紹介</a></li>
+    <li><a href="">私たちについて</a></li>
+    <li><a href="">事例</a></li>
+    <li><a href="">採用情報</a></li>
+    <li><a href="">よくある質問</a></li>
+    <li class="contact_link"><a href="">お問い合わせ</a></li>
+  </ul>
+  <a href="" class="footer_contact_btn">相談・お問い合わせはこちら</a>
+  <div class="footer_copyright">
+    <p>©lsam Inc. All Rights Reserved.</p>
+    <ul>
+      <li><a href="" class="footer_t"><img src="/wp-content/themes/lsam/img/icon_twitter.png" alt=""></a></li>
+      <li><a href="" class="footer_f"><img src="/wp-content/themes/lsam/img/icon_facebook.png" alt=""></a></li>
+    </ul>
+  </div>
+</footer>
--- wp/front-page.php
+++ wp/front-page.php
@@ -1,50 +1,5 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-
-<meta charset="UTF-8" />
-<title>ホーム</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="/wp-content/themes/lsam/css/common.css" type="text/css" media="screen">
-</head>
-<body>
-<header>
-  <div class="header_gnavi_bg">
-    <div class="header_gnavi">
-      <a href="http://localhost/" class="lsam_logo"><img src="/wp-content/themes/lsam/img/header_logo.png" alt=""></a>
-      <ul>
-        <li><a href="">サービス紹介</a></li>
-        <li><a href="">私たちについて</a></li>
-        <li><a href="">プライバシーポリシー</a></li>
-        <li><a href="">お問い合わせ</a></li>
-      </ul>
-      <a href="" class="sp_gnavi"><img src="/wp-content/themes/lsam/img/sp_gnavi.png" alt=""></a>
-    </div>
-  </div>
-  <div class="header_menu_box">
-    <ul class="header_gmenu">
-      <li class="link_doer"><a href=""><img src="/wp-content/themes/lsam/img/doer_logo_footer.png" alt=""></a></li>
-      <li class="link_new"><a href="">新着</a></li>
-      <li class="link_popular"><a href="">人気</a></li>
-      <li class="link_marketing"><a href="">マーケティング</a></li>
-      <li class="link_project"><a href="">プロジェクト推進</a></li>
-      <li class="link_ceo"><a href="">社長ブログ</a></li>
-      <li class="link_info"><a href="">お知らせ</a></li>
-      <li class="icon_search"><a href=""><img src="/wp-content/themes/lsam/img/icon_search.png" alt=""></a></li>
-    </ul>
-    <ul class="header_gnavi">
-      <li><a href="">トップ</a></li>
-      <li><a href="">サービス紹介</a></li>
-      <li><a href="">私たちについて</a></li>
-      <li><a href="">事例</a></li>
-      <li><a href="">採用情報</a></li>
-      <li><a href="">よくある質問</a></li>
-      <li class="contact_link"><a href="">お問い合わせ</a></li>
-    </ul>
-  </div>
-</header>
-
+<?php get_header(); ?>
 <main class="top_box">
   <div class="top_heroimg_bg">
     <div class="heroimg_box">
@@ -117,30 +72,7 @@
   </ul>
 
 </main>
-
-<footer class="main-footer">
-  <div class="footer_gmenu">
- 
-  </div>
-  <ul class="footer_gnavi">
-    <li><a href="">トップ</a></li>
-    <li><a href="">サービス紹介</a></li>
-    <li><a href="">私たちについて</a></li>
-    <li><a href="">事例</a></li>
-    <li><a href="">採用情報</a></li>
-    <li><a href="">よくある質問</a></li>
-    <li class="contact_link"><a href="">お問い合わせ</a></li>
-  </ul>
-  <a href="" class="footer_contact_btn">相談・お問い合わせはこちら</a>
-  <div class="footer_copyright">
-    <p>©lsam Inc. All Rights Reserved.</p>
-    <ul>
-      <li><a href="" class="footer_t"><img src="/wp-content/themes/lsam/img/icon_twitter.png" alt=""></a></li>
-      <li><a href="" class="footer_f"><img src="/wp-content/themes/lsam/img/icon_facebook.png" alt=""></a></li>
-    </ul>
-  </div>
-</footer>
-
+<?php get_footer(); ?>
 </body>
 
 </html>
