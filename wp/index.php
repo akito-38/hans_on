4b3ee360e6a733d80e0a39d607178d0a04bef375
miyamoto <m.akirabo@gmail.com>
2019-06-29 14:15:41 +0900

    WPトップページ表示

---
+++ wp/index.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8" />
+<title>ホーム</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="/wp-content/themes/lsam/css/common.css" type="text/css" media="screen">
+</head>
+
+<header>
+  <div class="header_gnavi_bg">
+    <div class="header_gnavi">
+      <a href="http://localhost/" class="lsam_logo"><img src="/wp-content/themes/lsam/img/header_logo.png" alt=""></a>
+      <ul>
+        <li><a href="">サービス紹介</a></li>
+        <li><a href="">私たちについて</a></li>
+        <li><a href="">プライバシーポリシー</a></li>
+        <li><a href="">お問い合わせ</a></li>
+      </ul>
+      <a href="" class="sp_gnavi"><img src="/wp-content/themes/lsam/img/sp_gnavi.png" alt=""></a>
+    </div>
+  </div>
+  <div class="header_menu_box">
+    <ul class="header_gmenu">
+      <li class="link_doer"><a href=""><img src="/wp-content/themes/lsam/img/doer_logo_footer.png" alt=""></a></li>
+      <li class="link_new"><a href="">新着</a></li>
+      <li class="link_popular"><a href="">人気</a></li>
+      <li class="link_marketing"><a href="">マーケティング</a></li>
+      <li class="link_project"><a href="">プロジェクト推進</a></li>
+      <li class="link_ceo"><a href="">社長ブログ</a></li>
+      <li class="link_info"><a href="">お知らせ</a></li>
+      <li class="icon_search"><a href=""><img src="/wp-content/themes/lsam/img/icon_search.png" alt=""></a></li>
+    </ul>
+    <ul class="header_gnavi">
+      <li><a href="">トップ</a></li>
+      <li><a href="">サービス紹介</a></li>
+      <li><a href="">私たちについて</a></li>
+      <li><a href="">事例</a></li>
+      <li><a href="">採用情報</a></li>
+      <li><a href="">よくある質問</a></li>
+      <li class="contact_link"><a href="">お問い合わせ</a></li>
+    </ul>
+  </div>
+</header>
+
+<body><main class="top_box">
+  <div class="top_heroimg_bg">
+    <div class="heroimg_box">
+      <div class="heroimg_box_">
+        <h1><br>作っただけでは<br>終わらないWEB制作</h1>
+
+        <a href="" class="default_btn">サービスの詳細へ</a>
+      </div>
+    </div>
+  </div>
+<!-- DOER NOTE -->
+
+<!-- お知らせ -->
+  <section class="title_box">
+    <h1 class="title_h1">お知らせ</h1>
+  </section>
+  <ul class="post_list_box">
+    <li class="post_list">
+      <a href="">
+      <div class="post_list_box">
+        <p class="category_name mb10">カテゴリネーム</p>
+        <h1 class="mb10">記事のタイトル文が入ります。記事のタイトル文が入ります。記事のタイトル文が入ります。…</h1>
+        <div class="post_list_status">
+          <ul class="post_tag flort_l">
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+          </ul>
+          <p class="post_date flort_r">2018.00.00</p>
+        </div>
+      </div>
+      <img src="../wp-content/themes/lsam/img/post_img1.png" alt="">
+      </a>
+    </li>
+    <li class="post_list">
+      <a href="">
+      <div class="post_list_box">
+        <p class="category_name mb10">カテゴリネーム</p>
+        <h1 class="mb10">記事のタイトル文が入ります。記事のタイトル文が入ります。記事のタイトル文が入ります。…</h1>
+        <div class="post_list_status">
+          <ul class="post_tag flort_l">
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+          </ul>
+          <p class="post_date flort_r">2018.00.00</p>
+        </div>
+      </div>
+      <img src="../wp-content/themes/lsam/img/post_img1.png" alt="">
+      </a>
+    </li>
+    <li class="post_list">
+      <a href="">
+      <div class="post_list_box">
+        <p class="category_name mb10">カテゴリネーム</p>
+        <h1 class="mb10">記事のタイトル文が入ります。記事のタイトル文が入ります。記事のタイトル文が入ります。…</h1>
+        <div class="post_list_status">
+          <ul class="post_tag flort_l">
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+            <li>タグテキスト</li>
+          </ul>
+          <p class="post_date flort_r">2018.00.00</p>
+        </div>
+      </div>
+      <img src="../wp-content/themes/lsam/img/post_img1.png" alt="">
+      </a>
+    </li>
+    
+  </ul>
+
+</main>
+
+<footer class="main-footer">
+  <div class="footer_gmenu">
+ 
+  </div>
+  <ul class="footer_gnavi">
+    <li><a href="">トップ</a></li>
+    <li><a href="">サービス紹介</a></li>
+    <li><a href="">私たちについて</a></li>
+    <li><a href="">事例</a></li>
+    <li><a href="">採用情報</a></li>
+    <li><a href="">よくある質問</a></li>
+    <li class="contact_link"><a href="">お問い合わせ</a></li>
+  </ul>
+  <a href="" class="footer_contact_btn">相談・お問い合わせはこちら</a>
+  <div class="footer_copyright">
+    <p>©lsam Inc. All Rights Reserved.</p>
+    <ul>
+      <li><a href="" class="footer_t"><img src="/wp-content/themes/lsam/img/icon_twitter.png" alt=""></a></li>
+      <li><a href="" class="footer_f"><img src="/wp-content/themes/lsam/img/icon_facebook.png" alt=""></a></li>
+    </ul>
+  </div>
+</footer>
+
+</body>
+
+</html>
