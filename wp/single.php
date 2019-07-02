4d5a21cf5be050e0900bf460fa44326e64355215
miya <m.akirabo@gmail.com>
2019-07-02 17:33:42 +0900

    single.php用意

---
+++ wp/single.php
@@ -0,0 +1,31 @@
+
+<?php get_header(); ?>
+
+<main class="post_box">
+
+  <div class="post_topimg">
+    <div class="post_status">
+      <p class="category_name">カテゴリネーム</p>
+      <p class="post_date">2018.00.00</p>
+    </div>
+    <img src="../wp-content/themes/lsam/img/eyecatch.png" alt="" class="eyecatch">
+    <ul class="post_tag mb20">
+      <li>タグテキスト</li>
+      <li>タグテキスト</li>
+      <li>タグテキスト</li>
+    </ul>
+    <h1 class="post_title">記事のタイトルが入ります。このテキストサイズは記事タイトル部分のみ使用しています。</h1>
+  </div>
+
+  <p class="post_text">本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br>本文テキスト例文。<br></p>
+
+  <ul class="post_transition">
+    <li><a href="">前へ</a></li>
+    <li><a href="">次へ</a></li>
+  </ul>
+</main>
+
+<?php get_footer(); ?>
+</body>
+
+</html>
